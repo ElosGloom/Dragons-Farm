bf090c6c8c3aa43ea3f41b70677177ab6b4027d3
ElosGloom <elos.gloom@gmail.com>
2024-05-15 23:08:04 +0300

    dragon buttons group

---
+++ Assets/Game/Scripts/Common/DragonButtonGroupElement.cs
@@ -0,0 +1,12 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Game.Scripts.Common
+{
+    public class DragonButtonGroupElement : MonoBehaviour
+    {
+        public TextMeshProUGUI buttonText;
+
+    }
+}
---
+++ Assets/Game/Scripts/Common/DragonButtonsGroup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.Scripts.Common
+{
+    public class DragonButtonsGroup : MonoBehaviour
+    {
+        [SerializeField] private HorizontalLayoutGroup layoutGroup;
+        [SerializeField] private List<DragonButtonGroupElement> buttonsList = new();
+        [SerializeField] private DragonButtonGroupElement button;
+
+
+        private readonly Array _dragonTypes = Enum.GetValues(typeof(DragonType));
+
+        private void Start()
+        {
+            FillButtonsGroup();
+        }
+
+        private void FillButtonsGroup()
+        {
+            for (int i = 0; i < _dragonTypes.Length; i++)
+            {
+                var newButton = Instantiate(button, layoutGroup.transform);
+                buttonsList.Add(newButton);
+
+                var type = (DragonType)_dragonTypes.GetValue(i);
+                newButton.buttonText.text = type.ToString();
+            }
+        }
+    }
+}
